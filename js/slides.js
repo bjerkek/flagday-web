2561ba58a432aee4d93313e94cf169fcdeee5f65
bjerkek <bjerkek@gmail.com>
2019-07-16 11:04:34 +0200

    styling

--- js/slides.js
+++ js/slides.js
@@ -24,4 +24,11 @@ function showImg(n) {
     x[i].style.display = "none";  
   }
   x[n].style.display = "block";
+
+  var j;
+  var y = document.getElementsByClassName("mySlides-button");
+  for (j = 0; j < y.length; j++) {
+    y[j].style.color = "#a3a3a7";
+  }
+  y[n].style.color = "#006666";
 }
