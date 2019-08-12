cba35facffdc0765f8355ad9c89d318cd1ca8858
bjerkek <bjerkek@gmail.com>
2019-08-12 11:39:22 +0200

    Removed unused code. Cleanup.

--- js/slides.js
+++ js/slides.js
@@ -1,34 +1,16 @@
-var slideIndex = 0;
+let slideIndex = 0;
 showImg(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-// function showDivs(n) {
-//   console.log(n)
-//   var i;
-//   var x = document.getElementsByClassName("mySlides");
-//   if (n > x.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = x.length}
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";  
-//   }
-//   x[slideIndex-1].style.display = "block";  
-// }
-
 function showImg(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  var x = document.getElementsByClassName('mySlides');
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';  
   }
-  x[n].style.display = "block";
+  x[n].style.display = 'block';
 
-  var j;
-  var y = document.getElementsByClassName("mySlides-button");
-  for (j = 0; j < y.length; j++) {
-    y[j].style.color = "#a3a3a7";
+  var y = document.getElementsByClassName('mySlides-button');
+  for (let j = 0; j < y.length; j++) {
+    y[j].style.color = '#a3a3a7';
   }
-  y[n].style.color = "#006666";
+  y[n].style.color = '#006666';
 }
