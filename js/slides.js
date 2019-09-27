3872377192a415162b09db276b42ba83d3cbcb64
bjerkek <bjerkek@gmail.com>
2019-09-27 11:57:53 +0200

    Linting. New colors

--- js/slides.js
+++ js/slides.js
@@ -1,16 +1,16 @@
-let slideIndex = 0;
-showImg(slideIndex);
+let slideIndex = 0
+showImg(slideIndex)
 
-function showImg(n) {
-  const x = document.getElementsByClassName('mySlides');
+function showImg (n) {
+  const x = document.getElementsByClassName('mySlides')
   for (let i = 0; i < x.length; i++) {
-    x[i].style.display = 'none';  
+    x[i].style.display = 'none'
   }
-  x[n].style.display = 'block';
+  x[n].style.display = 'block'
 
-  const y = document.getElementsByClassName('mySlides-button');
+  const y = document.getElementsByClassName('mySlides-button')
   for (let j = 0; j < y.length; j++) {
-    y[j].style.color = '#a3a3a7';
+    y[j].style.color = '#a3a3a7'
   }
-  y[n].style.color = '#006666';
+  y[n].style.color = '#538FEF'
 }
